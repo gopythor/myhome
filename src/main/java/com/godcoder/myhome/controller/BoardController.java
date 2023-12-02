9307923b930e5f01b8e446fa2e078a4347698ee5
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-12-02 14:00:32 +0900

    feat:Board Post and Validator function added Title : more than 2 less than 30 Content : No empty
    Varidator dependancy was added to pom.xml

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -3,12 +3,18 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -17,6 +23,8 @@ public class BoardController {
 
     private final BoardRepository boardRepository;
 
+    private final BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
@@ -24,4 +32,24 @@ public class BoardController {
         return "board/list";
     }
 
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null){
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
 }
--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -1,9 +1,12 @@
 package com.godcoder.myhome.model;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +15,10 @@ import lombok.Setter;
 @Setter
 public class Board {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
     private String content;
 }
