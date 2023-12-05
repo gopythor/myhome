ac28812b0f5b612e2e4fcc4466311a96020ff048
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-12-05 15:32:55 +0900

    feat:Board API controller added 1) Search functions are added    - Title or Contents    - Title(now not used) 2) Through API, post can be CRUD.

--- src/main/java/com/godcoder/myhome/repository/BoardRepository.java
+++ src/main/java/com/godcoder/myhome/repository/BoardRepository.java
@@ -1,10 +1,12 @@
 package com.godcoder.myhome.repository;
 
 import com.godcoder.myhome.model.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
